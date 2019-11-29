a62776c2dc2f232d88003453b6e8eeb43112d3fd
drodobyte <drodobyte@gmail.com>
2019-11-29 12:21:54 +0100

    change: null id -> new entity Instead of id == 0

--- src/main/kotlin/drodobytecom/core/kotlin/entity/Entity.kt
+++ src/main/kotlin/drodobytecom/core/kotlin/entity/Entity.kt
@@ -1,20 +1,16 @@
 package drodobytecom.core.kotlin.entity
 
 interface Entity {
-    companion object {
-        const val NEW = 0L
-    }
+    val id: Long?
 
-    val id: Long
     fun isSame(other: Entity) =
-        !isNew() && !other.isNew() && id == other.id && javaClass == other.javaClass
+        !isNew() && id == other.id && javaClass == other.javaClass
 
     fun isNew(): Boolean =
-        id == NEW
+        id == null
 }
 
-
-fun Iterable<Entity>.nextId() = map { it.id }.maxBy { it } ?: Entity.NEW + 1
+fun Iterable<Entity>.nextId() = map { it.id }.maxBy { it ?: 0 } ?: 0
 
 fun <T : Entity> MutableList<T>.replace(entity: T) {
     this[indexOfFirst { it.isSame(entity) }] = entity
