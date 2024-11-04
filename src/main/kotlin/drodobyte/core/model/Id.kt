ed17990629dc8dc5dbadf7956db029a22d51aa38
drodobyte <drodobyte@gmail.com>
2024-11-04 10:16:39 +0200

    Id implementors & switch to String Id

--- src/main/kotlin/drodobyte/core/model/Id.kt
+++ src/main/kotlin/drodobyte/core/model/Id.kt
@@ -2,11 +2,46 @@ package drodobyte.core.model
 
 import java.util.UUID
 
-typealias Id = UUID
+typealias Id = String
+
+//typealias Id = UUID
+//typealias Id = Long
+
+private val implementor = Implementor.Str
 
 /**
- * New random Id
+ * New random [Id]
  */
-val newId get() = UUID.randomUUID()
-val String.asId get() = UUID.fromString(this)
-val Number.asId get() = toString().asId
+val newId: Id get() = implementor.factory.id()
+val String?.asId: Id? get() = implementor.factory.id(this)
+val Number?.asId: Id? get() = implementor.factory.id(this)
+
+
+private enum class Implementor {
+    Uuid, Str, Long;
+
+    val factory
+        get() = when (this) {
+            Uuid -> UuidFactory
+            Str -> StringFactory
+            Long -> TODO()
+        }
+}
+
+private interface Factory {
+    fun id(): Id
+    fun id(id: String?): Id?
+    fun id(id: Number?): Id?
+}
+
+private val UuidFactory = object : Factory {
+    override fun id() = UUID.randomUUID().toString()
+    override fun id(id: String?) = id?.let { UUID.fromString(id).toString() }
+    override fun id(id: Number?) = id(id?.toString())
+}
+
+private val StringFactory = object : Factory {
+    override fun id() = UuidFactory.id()
+    override fun id(id: String?) = id
+    override fun id(id: Number?) = id(id?.toString())
+}
